4c80c36bffc2886ba93944a97054cb419562ab6b
Kush Patel <ktpatel3@myseneca.ca>
2022-09-13 18:32:42 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+/*********************************************************************************
+* BTI325 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name:Kush Patel Student ID: 104006218 Date: 2022-09-13
+*
+* Online (Cyclic) URL:
+* https://nice-sock-bee.cyclic.app/
+********************************************************************************/ 
+
+
+
+
 var express = require("express");
 var app = express();
 
