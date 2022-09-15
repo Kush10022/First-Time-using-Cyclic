8e8dd7485e6533911c074a91616a92063e28f411
Kush Patel <ktpatel3@myseneca.ca>
2022-09-15 10:08:15 -0400

    new server.js

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ var HTTP_PORT = process.env.PORT || 8080;
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
-
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res){
     res.send("Kush Pate student id: (104006218) <br /><a href='/about'>Go to the about page</a>");
